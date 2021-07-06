a0b447bf2bed4b897a1bb783c41b5654621f117b
remonts <monteiro-renan@hotmail.com>
2021-07-06 06:48:35 -0300

    fetched is changed imc data with queryselector.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
-console.log("fui carregado do main");
-
+//Alterar título do site
 var titulo = document.querySelector(".title");
 titulo.textContent = "Renan Monteiro Nutrição";
+
+//Buscando pacientes
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = (peso / (altura * altura));
+
+var tdIMC = paciente.querySelector(".info-imc");
+tdIMC.textContent = imc;

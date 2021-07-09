0f6bde1a03bc8d57cfaf7e9a6e00db9b720a78d6
remonts <monteiro-renan@hotmail.com>
2021-07-09 08:54:07 -0300

    implemented loop to, changed line color when error validation is true.

--- js/main.js
+++ js/main.js
@@ -3,30 +3,36 @@ var titulo = document.querySelector(".title");
 titulo.textContent = "Renan Monteiro Nutrição";
 
 //Buscando pacientes
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdIMC = paciente.querySelector(".info-imc");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdIMC = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso >= 500) {
-    pesoValido = false;
-    tdIMC.textContent = "Peso inválido.";
-}
+    var pesoValido = true;
+    var alturaValida = true;
 
-if (altura <= 0 || altura > 3.00) {
-    alturaValida = false;
-    tdIMC.textContent = "Altura inválida.";
-}
+    if (peso <= 0 || peso >= 500) {
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido.";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if (pesoValido && alturaValida) {
-    var imc = (peso / (altura * altura));
-    tdIMC.textContent = imc;
+    if (altura <= 0 || altura > 3.00) {
+        alturaValida = false;
+        tdIMC.textContent = "Altura inválida.";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoValido && alturaValida) {
+        var imc = (peso / (altura * altura));
+        tdIMC.textContent = imc.toFixed(2);
+    }
 }

4801f54bbd54391658026e1de08a00c7510c8a28
remonts <monteiro-renan@hotmail.com>
2021-07-09 07:49:18 -0300

    aplyed variations in variables weigth and heigth.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,22 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = (peso / (altura * altura));
-
 var tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent = imc;
+
+var pesoValido = true;
+var alturaValida = true;
+
+if (peso <= 0 || peso >= 500) {
+    pesoValido = false;
+    tdIMC.textContent = "Peso inválido.";
+}
+
+if (altura <= 0 || altura > 3.00) {
+    alturaValida = false;
+    tdIMC.textContent = "Altura inválida.";
+}
+
+if (pesoValido && alturaValida) {
+    var imc = (peso / (altura * altura));
+    tdIMC.textContent = imc;
+}

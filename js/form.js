afa1547baec4a5f5496ac441b2206370ce9cb048
remonts <monteiro-renan@hotmail.com>
2021-07-15 11:38:46 -0300

    finished form validation.

--- js/form.js
+++ js/form.js
@@ -25,11 +25,15 @@ botaoAdicionar.addEventListener("click", function (event) {
     // inserindo elemento tr como filho da tabela
     tabela.appendChild(pacienteTr);
     // limpar os campos do form
+    var mensagensErro = document.querySelector("#mensagems-erro");
+    mensagensErro.innerHTML = "";
     form.reset();
 });
 
 function exibeMensagemsDeErro(erros) {
     var ul = document.querySelector("#mensagems-erro");
+    ul.innerHTML = "";
+
     erros.forEach(erro => {
         var li = document.createElement("li");
         li.textContent = erro;
@@ -78,8 +82,12 @@ function validaPaciente(paciente) {
 
     var erros = [];
 
+    if (paciente.nome.length == 0) erros.push("O campo nome deve ser preenchido.")
     if (!validaPeso(paciente.peso)) erros.push("O peso é inválido.")
     if (!validaAltura(paciente.altura)) erros.push("A altura é inválida.")
+    if (paciente.gordura.length == 0) erros.push("O campo gordura deve ser preenchido.")
+    if (paciente.peso.length == 0) erros.push("O campo peso deve estar preenchido.")
+    if (paciente.altura.length == 0) erros.push("O campo altura deve estar preenchido.")
 
     return erros;
 }

3172326b05de8b683acc4b7668da54d93c694745
remonts <monteiro-renan@hotmail.com>
2021-07-20 09:10:23 -0300

    created logic to filter patients using regular expression.

--- js/filtra.js
+++ js/filtra.js
@@ -1,15 +1,16 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-    console.log(this.value);
+    
     var pacientes = document.querySelectorAll(".paciente");
 
     if (this.value.length > 0) {
         pacientes.forEach(paciente => {
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
     
-            if (nome != this.value) {
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
             } else {
                 paciente.classList.remove("invisivel");

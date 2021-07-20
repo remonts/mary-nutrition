cc0ce47ca9bf79b134ce1d7b03ab589e6440a076
remonts <monteiro-renan@hotmail.com>
2021-07-20 08:57:22 -0300

    created logic to filter patients.

--- js/filtra.js
+++ js/filtra.js
@@ -1,3 +1,23 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
-console.log(campoFiltro);
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        pacientes.forEach(paciente => {
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+    
+            if (nome != this.value) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        });
+    } else {
+        pacientes.forEach(paciente => {
+            paciente.classList.remove("invisivel");
+        });
+    }
+});
